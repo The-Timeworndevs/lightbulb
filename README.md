cf91ce1ae7d45d09680232f220cbeafc470b1a15
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2023-05-13 17:12:38 -0500

    Preparations for glowlamps

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# Fabric Example Mod
+# Lightbulb
+## Lighting up your Minecraft World
 
-## Setup
+Lightbulb is a small mod that adds a few items. It is reccomended to use REI or EMI to see the recipes
 
-For setup instructions please see the [fabric wiki page](https://fabricmc.net/wiki/tutorial:setup) that relates to the IDE that you are using.
+Current items include:
 
-## License
-
-This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+Glowblocks in all 16 colors!
--- src/main/java/net/timeworndevs/lightbulb/registry/Blocks.java
+++ src/main/java/net/timeworndevs/lightbulb/registry/Blocks.java
@@ -1,13 +1,24 @@
 package net.timeworndevs.lightbulb.registry;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
 import net.timeworndevs.lightbulb.Main;
 
+import java.util.function.ToIntFunction;
+
 public class Blocks {
+    private static ToIntFunction<BlockState> createLightLevelFromLitBlockState(int litLevel) {
+        return (state) -> {
+            return (Boolean)state.get(Properties.LIT) ? litLevel : 0;
+        };
+    }
+    private static ToIntFunction<BlockState> createLightLevelFromUnlitLitBlockState(int litLevel) {
+        return (state) -> {
+            return !(Boolean)state.get(Properties.LIT) ? litLevel : 0;
+        };
+    }
 
     public static void register(){
         sets(Main.MOD_ID,"white");
@@ -27,7 +38,10 @@ public class Blocks {
         sets(Main.MOD_ID,"pink");
         sets(Main.MOD_ID,"brown");
     }
+
     public static void sets(String modid, String setName){
         Registries.registerBlocks(new Identifier(modid, setName + "_glowblock"), new Block(AbstractBlock.Settings.of(Material.STONE).sounds(BlockSoundGroup.GLASS).luminance((state) -> {return 15;}).strength(0.3f)));
+        Registries.registerBlocks(new Identifier(modid, setName + "_glowlamp.json"), new RedstoneLampBlock(AbstractBlock.Settings.of(Material.REDSTONE_LAMP).sounds(BlockSoundGroup.GLASS).luminance(createLightLevelFromLitBlockState(15)).strength(0.3f)));
+        Registries.registerBlocks(new Identifier(modid, "inverted_" + setName + "_glowlamp.json"), new RedstoneLampBlock(AbstractBlock.Settings.of(Material.REDSTONE_LAMP).sounds(BlockSoundGroup.GLASS).luminance(createLightLevelFromUnlitLitBlockState(15)).strength(0.3f)));
     }
 }
